c2be842ce72ce0cc6ea6bdea6e4ca86470892ef9
Kuro-Rui <louisdominic80@gmail.com>
2022-07-17 16:02:35 +0700

    Why did I forget this

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="circles",
+    version="0.0.1a",
+    url="https://github.com/Kuro-Rui/Circles",
+    license="GPLv3",
+    author="Kuro-Rui",
+    author_email="louisdominic80@gmail.com",
+    description="An async osu! API v1 wrapper.",
+    packages="circles",
+    setup_requires=["aiohttp"],
+    keywords=["osu", "osu!", "osu!api"],
+)
